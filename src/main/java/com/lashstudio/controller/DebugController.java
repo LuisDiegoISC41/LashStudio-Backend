c9b9171c1e72fe49d5d48909685fab961e95d9da
LuisDiegoISC41 <luisdiego4358@gmail.com>
2026-03-27 12:43:29 -0600

    Actualizar DebugController y arreglar autenticación

--- src/main/java/com/lashstudio/controller/DebugController.java
+++ src/main/java/com/lashstudio/controller/DebugController.java
@@ -1,15 +1,13 @@
 package com.lashstudio.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/debug")
