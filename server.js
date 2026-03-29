a65d8b75f2def9590dcfe3bbf1c081af1c1d44dc
LuisDiegoISC41 <luisdiego4358@gmail.com>
2026-03-28 21:37:55 -0600

    Correcciones de errores CORS

--- server.js
+++ server.js
@@ -1,29 +1,19 @@
 require('dotenv').config();
-const app = require('./src/app');
-const sequelize = require('./src/config/database');
+const app = require('./src/app'); // <--- Verifica que la ruta sea esta
+const { sequelize } = require('./src/models'); // Importamos sequelize desde los modelos
 const initializeData = require('./src/config/dataInitializer');
 
 const PORT = process.env.PORT || 8080;
 
 async function startServer() {
     try {
-        // Conectar a la DB
         await sequelize.authenticate();
-        console.log('✅ Conexión a la base de datos establecida.');
-        
-        // Sincronizar modelos (Relaciones)
+        console.log('✅ DB Conectada');
         await sequelize.sync({ alter: false });
-        console.log('✅ Modelos sincronizados.');
-
-        // Re-encriptar contraseñas si es necesario
         await initializeData();
-
-        app.listen(PORT, () => {
-            console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
-        });
-    } catch (error) {
-        console.error('❌ Error al iniciar:', error);
+        app.listen(PORT, () => console.log(`🚀 Puerto: ${PORT}`));
+    } catch (e) {
+        console.error('❌ Error:', e);
     }
 }
-
 startServer();
