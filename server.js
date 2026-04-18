0f2815637f1eb30ba7c3341605d57fdadbd99048
LuisDiegoISC41 <luisdiego4358@gmail.com>
2026-04-17 21:16:12 -0600

    Correcciones en horario 3

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ async function startServer() {
         await sequelize.sync({ alter: true }); 
         console.log("✅ Estructura de tablas actualizada en Render");
 
+        // Aseguramos que las columnas de cita puedan aceptar bloques sin cliente ni servicio
+        await sequelize.query('ALTER TABLE cita ALTER COLUMN "ID_Cliente" DROP NOT NULL');
+        await sequelize.query('ALTER TABLE cita ALTER COLUMN "ID_Servicio" DROP NOT NULL');
+        console.log('✅ Columnas ID_Cliente e ID_Servicio ajustadas para permitir NULL');
+
         await initializeData();
         app.listen(PORT, () => console.log(`🚀 Puerto: ${PORT}`));
     } catch (e) {
